0286198d460f7245783d693643beb3bbb48e6616
Corbinian Grimm <pixma@pixma140>
2016-12-14 15:17:54 +0100

    Initial commit

---
+++ js/script.js
@@ -0,0 +1,67 @@
+// start page with last filter = 0; 0 is no filter
+var lastFilter = 0;
+
+function initialize() {
+	
+	
+	// check if cookie favorite filter is set
+	if(getCookie("lastFilter") == ""){
+		// set last filter to no filter
+		document.cookie="lastFilter=0";				
+	} else {
+		// get last filter
+		lastFilter = getCookie("lastFilter");
+	}
+		
+	// TODO always update last filter
+}
+
+function buttonSwitchKameraPressed() {			
+	alert("buttonSwitchKameraPressed");
+	
+}
+
+function buttonNewEntryPressed() {			
+	alert("buttonNewEntryPressed");
+	
+}
+
+function buttonSavePressed() {
+   alert("buttonSavePressed");
+   
+   // save images with date
+}
+
+function setFavoriteFilter() {
+	// set current filter to favorite filter cookie
+}
+
+function getDate() {
+	var date = new Date();
+
+	var day = date.getDate();
+	var month = date.getMonth() + 1;
+	var year = date.getFullYear();
+
+	if (month < 10) month = "0" + month;
+	if (day < 10) day = "0" + day;
+
+	var today = day + "." + month + "." + year;
+	
+	return today;
+}
+
+function getCookie(cname) {
+    var name = cname + "=";
+    var ca = document.cookie.split(';');
+    for(var i = 0; i < ca.length; i++) {
+        var c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {			
+            return c.substring(name.length, c.length);
+        }
+    }
+    return "";
+}
