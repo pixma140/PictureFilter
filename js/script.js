570d53a0e81fa13bfbdb8b98bbebd4d9daf0cfd2
Corbinian Grimm <pixma@pixma140>
2017-05-09 19:37:49 +0200

    add missing semicolon

--- js/script.js
+++ js/script.js
@@ -145,7 +145,7 @@ function grayscale(d) {
 		// CIE luminance for the RGB
 		// The human eye is bad at seeing red and blue, so we de-emphasize them.
 		var v = 0.2126*r + 0.7152*g + 0.0722*b;
-		d[i] = d[i+1] = d[i+2] = v
+		d[i] = d[i+1] = d[i+2] = v;
 	}
 	return d;
 }
